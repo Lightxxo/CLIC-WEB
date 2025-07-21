df8773e0f8c5b82462dc34059fd5bffc9e54bf1c
asif93-138 <asif93@student.sust.edu>
2025-07-21 19:19:32 +0600

    working on email verification..

--- Frontend/src/pages/Signup/Signup.tsx
+++ Frontend/src/pages/Signup/Signup.tsx
@@ -3,25 +3,34 @@ import { useState } from "react";
 
 const Signup = () => {
     const [error, setError] = useState("");
-    function emailInput(e:any) {
+    const [enteredEmail, setEnteredEmail] = useState(false);
+    function emailInput(e: any) {
         setError("");
-                if (e.key === "Enter") {
-                    const input = e.target as HTMLInputElement;
-                    if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value))) {
-                        setError("Please enter a valid email address!"); return;
-                    }
-                    fetch("http://localhost:5000/checkUser?email=" + input.value)
-                    .then(res => {
-                        console.log(res.status);
-                    })
-                }
+        if (e.key === "Enter") {
+            const input = e.target as HTMLInputElement;
+            if (!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value))) {
+                setError("Please enter a valid email address!"); return;
+            }
+            // fetch("http://localhost:5000/checkUser?email=" + input.value)
+            // .then(res => {
+            //     console.log(res.status);
+            // });
+            setEnteredEmail(true);
+        }
     }
     return (
         <div className="mt-20 text-center">
-            <p className="">Talk to people at online events <br /> The events pool the people we know you will clic with</p>
-            <p className="text-xl my-5">Enter your email address to apply for membership</p>
-            <Input type="email" className="w-1/2 m-auto p-5" onKeyDown={emailInput} placeholder="Email" />
-            {error != "" && <p className="text-left w-1/2 m-auto text-red-600">{error}</p>}
+            <p className="text-xl">talk to people at online events <br /> the events pool the people we know you will clic with</p>
+            {!enteredEmail ?
+                <section>
+                    <p className="my-5">Enter your email address to apply for membership</p>
+                    <Input type="email" className="w-1/2 m-auto p-5" onKeyDown={emailInput} placeholder="Email" />
+                    {error != "" && <p className="text-left w-1/2 m-auto text-red-600 text-xs">{error}</p>}
+                </section>
+                :
+                <section>
+                    <p className="my-5">We have sent you a verification code. Check your email (also spam).</p>
+                </section>}
         </div>
     );
 };
