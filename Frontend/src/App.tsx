a4b08ffbee990a05269a8e86f844413ee13af187
asif93-138 <asif93@student.sust.edu>
2025-07-21 14:44:39 +0600

    signup page added

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React, { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectedLayout from "./layouts/ProtectedLayout";
 import Landing from "./pages/Landing/Landing";
+import Signup from "./pages/Signup/Signup";
 // Pages
 // import Login from "./routes/Login";
 
@@ -22,6 +23,7 @@ export default function App() {
     <Routes>
       {/* Public route */}
       <Route path="/" element={<Landing />} />
+      <Route path="/signup" element={<Signup />} />
 
       {/*
       <Route path="/login" element={<Login />} />
