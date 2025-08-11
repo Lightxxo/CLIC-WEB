89e1196234c4db6dbf6dea032e8adfc13895697d
asif93-138 <asif93@student.sust.edu>
2025-08-11 19:39:04 +0600

    styling of navbar and login page done!

--- Frontend/src/components/Navbar/Navbar.tsx
+++ Frontend/src/components/Navbar/Navbar.tsx
@@ -33,31 +33,31 @@ export default function Navbar() {
         {/* Desktop nav buttons */}
         <div className="hidden space-x-4 md:flex">
           {data.token ? (
-            <Button variant="ghost" asChild>
-              <a href="/howitworks" className="txt-color">How it works</a>
+            <Button variant="ghost">
+              <a href="/howitworks" className="txt-color text-lg gill-sans-bold pb-1">How it works</a>
             </Button>
           ) : (
             <></>
           )}
           {data.token && localStorage.getItem("isApproved") ? (
-            <Button variant="ghost" asChild>
-              <a href="/pools" className="txt-color">Pools</a>
+            <Button variant="ghost">
+              <a href="/pools" className="txt-color text-lg gill-sans-bold pb-1">Pools</a>
             </Button>
           ) : (
             <></>
           )}
 
           {data.token ? (
-            <Button variant="ghost" className="cursor-pointer" onClick={logOut}>
+            <Button variant="ghost" className="cursor-pointer txt-color text-lg gill-sans-bold pt-1" onClick={logOut}>
               Log out
             </Button>
           ) : (
             <>
-              <Button variant="ghost" asChild>
-                <a href="/signup" className="txt-color nav-links">Sign up</a>
+              <Button variant="ghost">
+                <a href="/signup" className="txt-color text-lg gill-sans-bold pb-1">Sign up</a>
               </Button>
-              <Button variant="ghost" asChild>
-                <a href="/login" className="txt-color nav-links">Login</a>
+              <Button variant="ghost">
+                <a href="/login" className="txt-color text-lg gill-sans-bold pb-1">Login</a>
               </Button>
             </>
           )}
@@ -108,15 +108,15 @@ export default function Navbar() {
               transition={{ type: "tween", duration: 0.3 }}
             >
               {data.token ? (
-                <Button variant="ghost" asChild>
-                  <a href="/howitworks" className="txt-color">How it works</a>
+                <Button variant="ghost">
+                  <a href="/howitworks" className="txt-color text-2xl txt-color text-lg gill-sans-bold pb-1">How it works</a>
                 </Button>
               ) : (
                 <></>
               )}
               {data.token && localStorage.getItem("isApproved") ? (
-                <Button variant="ghost" asChild>
-                  <a href="/pools" className="txt-color">Pools</a>
+                <Button variant="ghost">
+                  <a href="/pools" className="txt-color text-2xl txt-color text-lg gill-sans-bold pb-1">Pools</a>
                 </Button>
               ) : (
                 <></>
@@ -131,11 +131,11 @@ export default function Navbar() {
                 </Button>
               ) : (
                 <>
-                  <Button variant="ghost" asChild>
-                    <a href="/signup" className="txt-color">Sign up</a>
+                  <Button variant="ghost">
+                    <a href="/signup" className="txt-color text-2xl txt-color text-lg gill-sans-bold pb-1">Sign up</a>
                   </Button>
-                  <Button variant="ghost" asChild>
-                    <a href="/login" className="txt-color">Login</a>
+                  <Button variant="ghost">
+                    <a href="/login" className="txt-color text-2xl txt-color text-lg gill-sans-bold pb-1">Login</a>
                   </Button>
                 </>
               )}
