b48d56dbd50c1432666cb850897118c30d09f698
asif93-138 <asif93@student.sust.edu>
2026-03-09 18:03:23 +0600

    landing page animation mobile responsive done

--- Frontend/src/pages/Landing/Landing.tsx
+++ Frontend/src/pages/Landing/Landing.tsx
@@ -7,29 +7,29 @@ export default function Landing() {
 
   return (
     <div>
-<Marquee autoFill={true} speed={100} className="bg-[#AA3EEE] py-5">
-  <img src={img_1} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={100} className="bg-[#AA3EEE] py-6">
+  <img src={img_1} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={250} className="bg-[#0024F8] py-5" direction="right">
-  <img src={img_2} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={250} className="bg-[#0024F8] py-6" direction="right">
+  <img src={img_2} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={100} className="bg-[#FF16D0] py-5">
-  <img src={img_1} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={100} className="bg-[#FF16D0] py-6">
+  <img src={img_1} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={200} className="bg-[#FF6600] py-5" direction="right">
-  <img src={img_4} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={200} className="bg-[#FF6600] py-6" direction="right">
+  <img src={img_4} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={150} className="bg-[#AA3EEE] py-5">
-  <img src={img_1} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={150} className="bg-[#AA3EEE] py-6">
+  <img src={img_1} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={250} className="bg-[#0024F8] py-5" direction="right">
-  <img src={img_2} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={250} className="bg-[#0024F8] py-6" direction="right">
+  <img src={img_2} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={100} className="bg-[#FF16D0] py-5">
-  <img src={img_1} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={100} className="bg-[#FF16D0] py-6">
+  <img src={img_1} className="mx-6 w-120" />
 </Marquee>
-<Marquee autoFill={true} speed={200} className="bg-[#FF6600] py-5" direction="right">
-  <img src={img_4} className="mx-10 w-70" />
+<Marquee autoFill={true} speed={200} className="bg-[#FF6600] py-6" direction="right">
+  <img src={img_4} className="mx-6 w-120" />
 </Marquee>
     </div>
   );
