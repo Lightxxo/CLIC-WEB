c5babb0fdc925d82e64fe8af9a6edaab0607bc9f
Lightxxo <mahir.tasin.dev@gmail.com>
2026-05-05 17:25:22 +0600

    Update how it works styles

--- Frontend/src/pages/HowItWorks/HowItWorks.tsx
+++ Frontend/src/pages/HowItWorks/HowItWorks.tsx
@@ -26,56 +26,64 @@ export default function HowItWorks() {
 
         <article className="">
           <h4 className="gill-sans-bold text-2xl">1. Sign up: Become a 2CC member</h4>
-          <p className="text-2xl">
-            Complete a questionnaire to become a Two Clic Club member. 
-            Membership gives you access to live online events at which you speak to a series on members we pooled 
-            together especially for that event. Once your membership is approved, simply sign up for any of the 
-            events – called “pools” – to start chatting.
-          </p>
+          <ul className="text-2xl list-disc pl-8">
+            <li>Complete a questionnaire to become a Two Clic Club member.</li>
+            <li>
+              Membership gives you access to live online events where you speak to a series of members we pooled
+              together for that event.
+            </li>
+            <li>Once your membership is approved, sign up for events ("pools") to start chatting.</li>
+          </ul>
           <br />
           <hr className="border-black" />
           <br />
-          <h4 className="gill-sans-bold text-2xl">2. Download the app : You need the app to attend a pool and use the in-tool messaging</h4>
-          <p className="text-2xl">
-            You can download the app after you sign up for membership or
-            register for an event.
-          </p>
+          <h4 className="gill-sans-bold text-2xl">2. Download the app: You need the app to attend a pool and use the in-tool messaging</h4>
+          <ul className="text-2xl list-disc pl-8">
+            <li>You can download the app after signing up for membership or when registering for an event.</li>
+          </ul>
           <br />
           <hr className="border-black" />
           <br />
-          <h4 className="gill-sans-bold text-2xl">3. Register for a pool: Connect from the inside out, through conversation, and not the outside in. </h4>
-          <p className="text-2xl">
-            The pools are live, online events, which last for one to one and a half hours. You will need to turn on your video camera so you can partake in a series of short video dates or “bubbles”, in which you’ll speak to the members of the opposite sex consecutively. 
-The guestlist is curated from our pool of members. You can also share pool details with any friends you would like to bring along with you.
-
-          </p>
+          <h4 className="gill-sans-bold text-2xl">3. Register for a pool: Connect from the inside out, through conversation, and not the outside in</h4>
+          <ul className="text-2xl list-disc pl-8">
+            <li>Pools are live, online events lasting one to one and a half hours.</li>
+            <li>You must turn on your video camera to participate.</li>
+            <li>
+              You&apos;ll join a series of short video dates ("bubbles") with members of the opposite sex
+              consecutively.
+            </li>
+            <li>The guest list is curated from our pool of members.</li>
+            <li>You can share pool details with friends you&apos;d like to bring along.</li>
+          </ul>
           <br />
           <hr className="border-black" />
           <br />
-          <h4 className="gill-sans-bold text-2xl">4. Jump into the pool </h4>
-          <p className="text-2xl">
-            Log into the pool through the app on time and Tturn on your video camera and show up on time. 
-Once in the app, gGo to the pools page (wave icon) in the App and press Join. 
-Relax and be yourself. The app will start  to presenting members for you to video-date consecutively in bubbles of two person.
-
-          </p>
+          <h4 className="gill-sans-bold text-2xl">4. Join the pool</h4>
+          <ul className="text-2xl list-disc pl-8">
+            <li>Log into the pool through the app on time and turn on your video camera.</li>
+            <li>Go to the pools page (wave icon) and press Join.</li>
+            <li>Relax and be yourself.</li>
+            <li>The app will present members for consecutive two-person video-date "bubbles".</li>
+          </ul>
           <br />
           <hr className="border-black" />
           <br />
           <h4 className="gill-sans-bold text-2xl">5. Did you Clic?</h4>
-          <p className="text-2xl">
-            Each video date – or bubble of two  and pool has a fixed duration of 2 or 3 minutes (this will vary depending on each pool/event). If youu Clic  enjoywith your date/bubble, you’ll have the option to extend it. If the other person feels the samey also Clic’d with you, the date will be extended. At the end of the date or permitted extensions, each person on the bubble will be asked if they Clic’d with each other. If so, it will give them the right to message each other in the app after the event.
-RBut remember that each pool also has a fixed duration. So extending your bubble woth one person could extension could reduce the time available to speak in other bubbles to others in the pool.
-
-          </p>
+          <ul className="text-2xl list-disc pl-8">
+            <li>Each video date ("bubble") lasts 2-3 minutes (varies by pool/event).</li>
+            <li>You can extend the date if both participants agree.</li>
+            <li>After the date (or extensions), both participants are asked if they "Clic&apos;d".</li>
+            <li>If both say yes, you can message each other in the app after the event.</li>
+            <li>Pools have a fixed duration, so extending one bubble may reduce time for others.</li>
+          </ul>
           <br />
           <hr className="border-black" />
           <br />
           <h4 className="gill-sans-bold text-2xl">6. Feedback</h4>
-          <p className="text-2xl">
-            As we work on improving your experience, we would love to hear your
-            thoughts. Please complete the short feedback form after each date.
-          </p>
+          <ul className="text-2xl list-disc pl-8">
+            <li>Help us improve your experience.</li>
+            <li>Complete the short feedback form after each date.</li>
+          </ul>
           <br />
           {/* <hr className="border-black" /> */}
           <br />
