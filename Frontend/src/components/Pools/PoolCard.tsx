6b65388a094ab2a57f212b97b1a6665b3a78c7d7
asif93-138 <asif93@student.sust.edu>
2026-06-10 16:13:57 +0600

    pools page logic updated

--- Frontend/src/components/Pools/PoolCard.tsx
+++ Frontend/src/components/Pools/PoolCard.tsx
@@ -19,14 +19,16 @@ export function PoolCard({ pool }: { pool: Pool }) {
     ? pool.imgURL.slice(1)
     : pool.imgURL;
   const fullImageUrl = `https://twoclicclub.ams3.cdn.digitaloceanspaces.com/${cleanPath}`;
-
+  const flag = new Date(pool.date_time).getTime() > Date.now();
   return (
     <motion.div
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
+      // initial={{ opacity: 0, y: 20 }}
+      // animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
-      onClick={() => navigate(`/pools/${pool._id}`)}
-      className="cursor-pointer bg-[#D9D9D9] p-5 shadow-[0_3px_#8c8c8c]"
+      onClick={() => {
+        if (flag) navigate(`/pools/${pool._id}`);
+      }}
+      className={`${flag ? "cursor-pointer" : "opacity-50"} bg-[#D9D9D9] p-5 shadow-[0_3px_#8c8c8c]`}
     >
           <img src={fullImageUrl} />
               <h2 className="text-2xl font-bold truncate mb-4">
